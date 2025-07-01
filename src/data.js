481cd42afdd1e77b10fc58fc9f528d4dba58d082
Nigel Soh <nigelsoh@vwa.edu.sg>
2025-07-01 08:45:19 +0800

    Updated text to 'Annual Leave Remaining' in StaffView.js

--- src/data.js
+++ src/data.js
@@ -1,29 +1,29 @@
 export const leaveData = [
-  { name: "20220034", balance: 5, mc: 13 }, //Jessin
-  { name: "20230048", balance: 12.5, mc: 12 }, //Xueqi
-  { name: "20060001", balance: 22, mc: 14 }, //leiyu
-  { name: "20220030", balance: 9, mc: 12 }, //Mengyuan
-  { name: "20220038", balance: 13, mc: 10 }, //Jinghan
-  { name: "20220032", balance: 9, mc: 12 }, // Huang Qi
-  { name: "20230050", balance: 11.5, mc: 13 }, //Haohan
-  { name: "20230054", balance: 8, mc: 13 }, //Tian Tian
-  { name: "20230059", balance: 11, mc: 12.5 }, //Joshua
-  { name: "20230060", balance: 15.5, mc: 14 }, // Mingrui
-  { name: "20240065", balance: 13, mc: 11 }, //chiewseng
-  { name: "20240070", balance: 1, mc: 12 }, //Guo Qian
-  { name: "20240072", balance: 9, mc: 7.5 }, //Zhengyang
-  { name: "20240073", balance: 9, mc: 14 },// KangYing
-  { name: "20240076", balance: 17, mc: 5 },//Kimberley
-  { name: "20240075", balance: 8.5, mc: 5 }, //Benjamin
-  { name: "20240080", balance: 9, mc: 10 }, //Hsin Yu
+  { name: "20220034", balance: 6, mc: 13 }, //Jessin
+  { name: "20230048", balance: 8.5, mc: 12 }, //Xueqi
+  { name: "20060001", balance: 11, mc: 13.5 }, //leiyu
+  { name: "20220030", balance: 5, mc: 10 }, //Mengyuan
+  { name: "20220038", balance: 11, mc: 10 }, //Jinghan
+  { name: "20220032", balance: 4, mc: 11 }, // Huang Qi
+  { name: "20230050", balance: 10.5, mc: 12 }, //Haohan
+  { name: "20230054", balance: 6, mc: 10 }, //Tian Tian
+  { name: "20230059", balance: 6, mc: 12.5 }, //Joshua
+  { name: "20230060", balance: 10.5, mc: 14 }, // Mingrui
+  { name: "20240065", balance: 12, mc: 11 }, //chiewseng
+  { name: "20240070", balance: 0, mc: 12 }, //Guo Qian
+  { name: "20240072", balance: 4, mc: 6.5 }, //Zhengyang
+  { name: "20240073", balance: 10, mc: 14 },// KangYing
+  { name: "20240076", balance: 7, mc: 4 },//Kimberley
+  { name: "20240075", balance: 5.5, mc: 2 }, //Benjamin
+  { name: "20240080", balance: 5, mc: 9 }, //Hsin Yu
   { name: "20240082", balance: 5, mc: 8 }, //Miao En
   { name: "20240079", balance: 18, mc: 13 }, //Wu nan
-  { name: "20240078", balance: 11, mc: 14 }, // zhanghui
-  { name: "20240083", balance: 21, mc: 14 }, //Xiao Xuan
-  { name: "20250085", balance: 11, mc: 14 }, //Ken Fui
-  { name: "20250086", balance: 3, mc: 14 }, //Lin Chunbo 
-  { name: "20250087", balance: 15, mc: 14 }, //Nigel
-  { name: "20250088", balance: 14, mc: 14 }, // Ginny
+  { name: "20240078", balance: 12, mc: 14 }, // zhanghui
+  { name: "20240083", balance: 0, mc: 0 }, //Xiao Xuan
+  { name: "20250085", balance: 9, mc: 14 }, //Ken Fui
+  { name: "20250086", balance: 3, mc: 13 }, //Lin Chunbo 
+  { name: "20250087", balance: 13, mc: 14 }, //Nigel
+  { name: "20250088", balance: 1, mc: 14 }, // Ginny
   { name: "20258888", balance: 10, mc: 8 } // Demo
 
   ,
