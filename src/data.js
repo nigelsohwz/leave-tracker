7232199f8ec12ff2d9c76cfdca50a3367d9c916c
Nigel Soh <nigelsoh@vwa.edu.sg>
2025-09-10 10:45:57 +0800

    Updated text to 'Annual Leave Remaining' in StaffView.js

--- src/data.js
+++ src/data.js
@@ -1,15 +1,15 @@
 export const leaveData = [
-  { name: "20220034", balance: 7, mc: 13 }, //Jessin
-  { name: "20230048", balance: 8, mc: 11 }, //Xueqi
-  { name: "20060001", balance: 11, mc: 13.5 }, //leiyu
-  { name: "20220030", balance: 5, mc: 10 }, //Mengyuan
+  { name: "20220034", balance: 7, mc: 10 }, //Jessin
+  { name: "20230048", balance: 7, mc: 11 }, //Xueqi
+  { name: "20060001", balance: 8, mc: 13.5 }, //leiyu
+  { name: "20220030", balance: 5, mc: 8.5 }, //Mengyuan
   { name: "20220038", balance: 0, mc: 0 }, //Jinghan
-  { name: "20220032", balance: 4, mc: 10 }, // Huang Qi
-  { name: "20230050", balance: 10.5, mc: 10 }, //Haohan
-  { name: "20230054", balance: 5, mc: 10 }, //Tian Tian
-  { name: "20230059", balance: 6, mc: 11.5 }, //Joshua
+  { name: "20220032", balance: 4, mc: 9 }, // Huang Qi
+  { name: "20230050", balance: 10, mc: 10 }, //Haohan
+  { name: "20230054", balance: 5, mc: 9 }, //Tian Tian
+  { name: "20230059", balance: 5, mc: 11.5 }, //Joshua
   { name: "20230060", balance: 10.5, mc: 14 }, // Mingrui
-  { name: "20240065", balance: 11.5, mc: 10 }, //chiewseng
+  { name: "20240065", balance: 11, mc: 10 }, //chiewseng
   { name: "20240070", balance: 0, mc: 11 }, //Guo Qian
   { name: "20240072", balance: 4, mc: 5.5 }, //Zhengyang
   { name: "20240073", balance: 10, mc: 14 },// KangYing
@@ -17,13 +17,13 @@ export const leaveData = [
   { name: "20240075", balance: 0, mc: 0 }, //Benjamin
   { name: "20240080", balance: 5, mc: 8 }, //Hsin Yu
   { name: "20240082", balance: 6, mc: 8 }, //Miao En
-  { name: "20240079", balance: 9, mc: 5 }, //Wu nan
+  { name: "20240079", balance: 9, mc: 4 }, //Wu nan
   { name: "20240078", balance: 12, mc: 14 }, // zhanghui
   { name: "20240083", balance: 0, mc: 0 }, //Xiao Xuan
   { name: "20250085", balance: 9, mc: 14 }, //Ken Fui
   { name: "20250086", balance: 1, mc: 13 }, //Lin Chunbo 
   { name: "20250087", balance: 13, mc: 14 }, //Nigel
-  { name: "20250088", balance: 9, mc: 14 }, // Ginny
+  { name: "20250088", balance: 7, mc: 14 }, // Ginny
   { name: "20250089", balance: 0, mc: 14 }, // Tabitha
   { name: "20250090", balance: 0, mc: 14 }, // Xu Weiyi
   { name: "20258888", balance: 10, mc: 8 } // Demo
